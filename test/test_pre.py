5f7c5be817a566d2e71884f2125508c6412714b5
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2018-10-11 21:59:27 +0900

    MOD: add test case for test excel file

--- test/test_pre.py
+++ test/test_pre.py
@@ -1,10 +1,29 @@
 import unittest
+import pandas as pd
 from src.pre import pre
 
 class TestStringMethods(unittest.TestCase):
   def test_pre(self):
-    filepath = './data/test_book.xlsx'
-    self.assertEqual(pre.main(filepath), filepath)
+    filepath = './test/data/test_book.xlsx'
+    headers = ['Item', 'Default', 'Val*']
+
+    expected_sheet1 = pd.DataFrame({
+      'Item': ['AAA', 'BBB', 'CCC', 'DDD', 'EEE'],
+      'Default': ['aaa', 'bbb', '', 'ddd', 'eee'],
+      'Val1': ['xxx', 'xxx', '', 'ddd', 'eee'],
+      'Val2': ['aaa', 'bbb', '', 'ddd', 'eee'],
+      'Val3':  ['aaa', 'bbb', '', 'xxx', 'xxx']
+    })
+    expected_sheet2 = pd.DataFrame({
+      'Item': ['FFF', 'GGG', 'HHH'],
+      'Default': ['fff', '', 'hhh'],
+      'Val1': ['fff', '', 'hhh'],
+      'Val2': ['fff', '', 'hhh']
+    })
+
+    actual = pre.main(filepath, headers)
+    self.assertTrue(actual[0].equals(expected_sheet1))
+    self.assertTrue(actual[1].equals(expected_sheet2))
 
 if __name__ == '__main__':
     unittest.main()
